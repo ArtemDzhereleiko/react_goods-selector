65525dfb4eb7b9a33ad15d962abf6eec4d2658a7
Artem Dzhereleiko <dgerik2015@gmail.com>
2021-07-19 22:04:34 +0300

    Updates

---
+++ precache-manifest.4b8abdfcaa5f985645022cc35db3a7da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dab6c2fce63ef741f62fe5fe1ea5058",
+    "url": "./index.html"
+  },
+  {
+    "revision": "10ad593f85dce35e9361",
+    "url": "./static/css/main.75b50585.chunk.css"
+  },
+  {
+    "revision": "423d1f8f07466d4c3b37",
+    "url": "./static/js/2.0704ab71.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.0704ab71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10ad593f85dce35e9361",
+    "url": "./static/js/main.ac6b62a7.chunk.js"
+  },
+  {
+    "revision": "f861d31e3669d4c8fef0",
+    "url": "./static/js/runtime-main.c970f6dd.js"
+  }
+]);
